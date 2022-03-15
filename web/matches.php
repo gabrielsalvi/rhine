1cd3df3cb688470496f3c7d4db1cf9b0174b930f
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-03-15 20:34:42 -0300

    feat: add the matches screen

---
+++ web/matches.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="public/css/navbar.css">
+
+    <title>Matches</title>
+</head>
+<body>
+    <div class="grid-container">
+        <nav>
+            <div class="website-logo">
+                <span>Logo do Site</span>
+            </div>
+            <div class="search-field">
+                <input id="input-search" type="search" placeholder="Buscar partida">
+                <img src="public/img/icons/searcher-white-50x50.png" alt="search">
+            </div>
+            <div class="filter-button">
+                <a href="" title="Filtrar" target="_blank"><img src="public/img/icons/filter-50x50.png" alt="filtrar"></a>
+            </div>
+            <div class="profile-button">
+                <a href="" title="Profile" target="_blank"><img src="public/img/icons/default-profile-66x66.png" alt="profile"></a>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
