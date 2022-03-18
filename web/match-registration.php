f240cb0ff324b702e8b0268e770900f0e623ab11
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-03-18 19:33:44 -0300

    feat: add the match registration screen

---
+++ web/match-registration.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="public/css/form.css">
+    <link rel="stylesheet" href="public/css/match-registration.css">
+
+    <title>Oferte uma Partida! | {Nome do Site}</title>
+</head>
+<body>
+    <div class="form-container">
+        <form id="match-registration-form" action="" method="post">
+            <h1>Dados da Partida</h1>
+            <label for="sports-list">Esporte:</label>
+            <select name="sports-list">
+                <option value="seven-a-side-footbal">Futebol Society</option>
+                <option value="futsal">Futsal</option>
+                <option value="volleyball">Vôlei</option>
+            </select>
+
+            <label for="match-date">Data:</label>
+            <input type="date" name="match-date" required/>
+
+            <label for="start-time">Horário de Início:</label>
+            <input type="time" name="start-time" min="00:00" max="23:59" required/>
+
+            <label for="end-time">Horário de Término:</label>
+            <input type="time" name="end-time" required/>
+
+            <label for="price">Valor (R$): </label>
+            <input type="text" name="price" placeholder="15,00" required/>
+            
+            <input type="submit" value="Ofertar"/>
+        </form>
+    </div>
+</body>
+</html>
