1955c30a62c47d1ed63d55e14143c61fc5cae144
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-03-19 09:50:50 -0300

    feat: add sport center profile screen

---
+++ web/sport-center.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="public/css/profile.css">
+
+    <title>Profile</title>
+</head>
+<body>
+    <div class="profile-container">
+        <div class="banner"></div>
+        <div class="profile-photo-area"></div>
+        <div class="profile-info">
+            <span class="name">Titi</span>
+            <span class="username">@titi</span>
+        </div>
+        <div class="description">
+            <span>
+                Venha se divertir jogando futsal e futebol society aqui no Centro Esportivo Titi.
+                Venha se divertir jogando futsal e futebol society aqui no Centro Esportivo Titi.
+            </span>
+        </div>
+        <div class="working-time" title="Horário de Funcionamento">
+            <img src="public/img/icons/clock-48x48.png">
+            <span>14h00 - 23h59</span>
+        </div>
+    </div>
+</body>
+</html>
