c57b226a75af73b7c3f341fa234484461df747cd
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-03-19 23:28:38 -0300

    fix: remove unused tailwind css script

--- web/athlete-registration.php
+++ web/athlete-registration.php
@@ -7,8 +7,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <script src="https://cdn.tailwindcss.com"></script>
-
         <link rel="stylesheet" href="public/css/main.css">
         <link rel="stylesheet" href="public/css/form.css">
         <link rel="stylesheet" href="public/css/athlete-registration.css">
