4e603c7f00ccb95f0af0de86f211c242ffb84d77
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2022-03-29 22:37:14 -0300

    feat: allow athlete registration into the database

--- web/athlete-registration.php
+++ web/athlete-registration.php
@@ -1,7 +1,45 @@
+<?php
+
+    require_once 'database.php';
+
+    if (validateForm()) {
+
+        $db = connect();
+
+        $sql = 'INSERT INTO atletas (cpf, nome, sobrenome, dtnascimento, username, email, senha) 
+                    VALUES (:cpf, :firstname, :lastname, :birthdate, :username, :email, :_password);';
+ 
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':cpf', $_POST['cpf']);
+        $stmt->bindParam(':firstname', $_POST['first-name']);
+        $stmt->bindParam(':lastname', $_POST['last-name']);
+        $stmt->bindParam(':birthdate', $_POST['birthdate']);
+        $stmt->bindParam(':username', $_POST['username']);
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->bindParam(':_password', $_POST['password']);
+
+        $stmt->execute();
+
+    }
+
+    function validateForm() : bool {
+
+        if(isset($_POST['cpf']) && isset($_POST['first-name']) && isset($_POST['last-name']) && isset($_POST['birthdate']) 
+            && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])) {
+                return true;
+        }
+
+        return false;
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Cadastre-se | {Nome do Site}</title>
+        <title>Cadastre-se | Rhine </title>
 
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,6 +61,9 @@
                 
                 <label for="birthdate">Data de Nascimento:</label>
                 <input type="date" name="birthdate" required/>
+
+                <label for="cpf">CPF:</label>
+                <input type="text" name="cpf" required/>
                 
                 <label for="email">Email:</label>
                 <input type="email" name="email" required/>
